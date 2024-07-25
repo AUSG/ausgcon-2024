4ccd3f82bb13517fa47fb643403dd835d0e3b8b7
Geun-Oh <kandy1002@naver.com>
2024-07-25 22:46:04 +0900

    feat: add background color with gradient

--- src/Introduce.tsx
+++ src/Introduce.tsx
@@ -1,11 +1,11 @@
 const Introduce = () => {
     return <div className="h-[100vh] flex flex-col gap-[80px] text-white">
-        <div className="mt-[8rem]">
+        <div className="mt-[8rem] animate-appear-first">
             <p className="text-[3rem] font-semibold">AUSGCON 2024</p>
             <p className="text-[2rem] font-semibold">: try-catch</p>
         </div>
-        <img src="cloud_1.png" alt="cloud_1" className="animate-appear" />
-        <button type="button" className="bg-white text-black rounded-[20px] font-semibold">신청하러 가기</button>
+        <img src="cloud_1.png" alt="cloud_1" className="animate-appear-second max-w-[400px] self-center" />
+        <button type="button" className="w-[300px] self-center bg-white text-black rounded-[20px] font-semibold animate-appear-third border-none hover:bg-gray-900 hover:text-white">신청하러 가기</button>
     </div>
 }
 
